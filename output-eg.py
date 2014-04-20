ca7c209761968322af30137fe53e5a64f979aaeb
rookiehugo <yinhanchun24@gmail.com>
2014-04-20 16:01:08 +0800

    input raw_input(removed in python 3.?)

--- output-eg.py
+++ output-eg.py
@@ -1,6 +1,6 @@
-name = input("What is your name? ")
+name = raw_input("What is your name? ")
 age = input("How old are you? ")
-nationality = input("Where are you from? ")
+nationality = raw_input("Where are you from? ")
 
 print("Ah, so your name is %s, you are %s years old, " \
 "and you come from %s." % (name, age, nationality))
